283a5963f0733811fab9bc7818e4f1997255f108
sagargpatil62 <69417099+sagargpatil62@users.noreply.github.com>
2020-11-10 08:55:39 +0530

    Add files via upload

---
+++ BC_PROJECT/functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+#define SCALE 35
+
+int compareEqual(number a, number b);
+void decimalEqual(number *a, number *b);
+number *add(number *a, number *b);
+number *sub(number *a, number *b);
+number *mult(number *a, number *b);
+number *division(number *a, number *b);
+number *modulus(number *a, number *b);
+
+#endif // FUNCTIONS_H_INCLUDED
